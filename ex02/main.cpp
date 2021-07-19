c425fb7daa8dc507e809808441e4c98c42f68e68
lince <lincerpi@student.42roma.it>
2021-07-19 15:37:32 +0200

    C++08

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,4 +1,5 @@
 #include "mutantstack.hpp"
+#include <list>
 
 int main()
 {
@@ -23,5 +24,67 @@ int main()
 	++it;
 	}
 	std::stack<int> s(mstack);
+
+    std::cout << "\nLIST" << std::endl;
+
+
+    std::list<int>    list;
+
+    list.push_back(15);
+    list.push_back(100);
+
+    std::cout << "TOP = " << list.back() << std::endl;
+
+    list.pop_back();
+
+    std::cout << "Size = " << list.size() << std::endl;
+
+    list.push_back(8);
+    list.push_back(16);
+    list.push_back(258);
+
+    //[...]
+    list.push_back(1024);
+
+    std::list<int>::iterator itlist = list.begin();
+    std::list<int>::iterator itliste = list.end();
+
+    ++itlist;
+    --itlist;
+
+    while(itlist != itliste){
+        std::cout << *itlist << std::endl;
+        ++itlist;
+    }
+    
+       std::cout << "\nSTACK" << std::endl;
+
+
+    MutantStack<int>    s3;
+
+    s3.push(15);
+    s3.push(100);
+
+    std::cout << "TOP = " << s3.top() << std::endl;
+
+    s3.pop();
+
+    std::cout << "Size = " << s3.size() << std::endl;
+
+    s3.push(8);
+    s3.push(16);
+    s3.push(258);
+
+    //[...]
+    s3.push(1024);
+
+    MutantStack<int>::iterator itstack = s3.begin();
+    MutantStack<int>::iterator itstacke = s3.end();
+
+    while(itstack != itstacke){
+        std::cout << *itstack << std::endl;
+        ++itstack;
+    }
+    
 	return 0;
 }
